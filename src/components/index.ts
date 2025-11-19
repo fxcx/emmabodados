49ce24a209e65f1dac64ed995f61f56b1b7716cf
fxcx <facundo19burgos@gmail.com>
2025-11-19 18:59:00 -0300

    base de componenetes sobre nosotros y services

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,9 @@ export { Popup } from './popup/popup';
 export { Header } from './header/header';
 export { Hero } from './home/hero/hero';
 export { default as Nav } from './home/nav/nav';
-export { Footer} from './footer/footer';
-export {Logo} from './home/logo/logo';
-export {Form} from './form/form';
+export { Footer } from './footer/footer';
+export { Logo } from './home/logo/logo';
+export { Form } from './form/form';
+// export {ServiceCard} from "./cards/serviceCard" 
+// export {} from "./cards/profileCard.tsx" 
+// export {} from "./cards/cardList.tsx" 
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,15 +1,23 @@
 import { Hero } from "@/components/home/hero/hero"
-import {Form} from "@/components" // se puede importar directamente con components
+import { Form } from "@/components" // se puede importar directamente con components
+// import { Services } from "@/components/cards/serviceCard" 
 
-export default function Home () {
-    return (
-        <section>
-            <div>
-                <Hero/>
-            </div>
-            <section>
-                <Form/>
-            </section>
-        </section>
-    )
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <Hero />
+      </div>
+      <article>
+        <h2>Componenete servicio</h2>
+        {/* <Services/> */}
+      </article>
+      <section>
+        <h2>Sobre nosotros</h2>
+      </section>
+      <section>
+        <Form />
+      </section>
+    </main>
+  )
 }
