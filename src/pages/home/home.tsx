d58443db30dfa53ce8f2601b1b18be86b251f966
fxcx <facundo19burgos@gmail.com>
2025-11-20 19:24:03 -0300

    agregue imagenes, abot movile, excritorio y arreglo de logo del footer

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "@/components/hero/hero";
 import { Form } from "@/components"; // se puede importar directamente con components
 import { Services } from "@/components/cards/Services";
+import {About} from "@/components/about"
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
         <Services />
       </article>
       <section>
-        <h2>Sobre nosotros</h2>
+        <About/>
       </section>
       <section>
         <Form />
--- src/types/imgtype.ts
+++ src/types/imgtype.ts
@@ -1,8 +1,13 @@
-import image1 from "../assets/img/1242fea349e9432609cde2ecba378245919d56db.jpg"
-import logo from "../assets/img/e98829bd5f0e96861a2f902cf2f1957ed0772c95.png"
-import heroImg from "../assets/img/heroImg.png"
+import logo from "@/assets/img/e98829bd5f0e96861a2f902cf2f1957ed0772c95.png"
+import heroImg from "@/assets/img/heroImg.png"
+import fondoAbout from "@/assets/img/fondo about.png"
+import logoFooter from "@/assets/img/logo-footer.png"
+import francisco from "@/assets/img/francisco.jpg"
+import julieta from "@/assets/img/julieta.jpg"
+import pablo from "@/assets/img/pablo.jpg"
 import heroImgMobile from "../assets/img/heroImgMobile.png"
 import nombres from "../assets/img/nombres.png"
+import image1 from "../assets/img/1242fea349e9432609cde2ecba378245919d56db.jpg"
 
 
 export type ImageT = {
@@ -14,8 +19,10 @@ export type ImageT = {
 
 export const allImages = {
     "logo": logo,
-    "image1": image1,
     "heroImg": heroImg,
-    "heroImgMobile": heroImgMobile,
-    "nombres": nombres,
+    "fondoAbout": fondoAbout,
+    "logoFooter": logoFooter,
+    "francisco": francisco,
+    "julieta": julieta,
+    "pablo": pablo,
 }
